2207eb58aa0b8b14046cbd18ad7a75bc95df8eb5
SavchukValeria <V.Savchuk_FIT_12_21_B_d@knute.edu.ua>
2022-10-04 16:20:59 +0300

    1
    1

---
+++ ClassLibrary8/User/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace ClassLibrary4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User solution\n");
+
+            User ezUser = new User();
+
+            ezUser.Vivod();
+
+            Console.ReadKey();
+        }
+    }
+    class User
+    {
+        private string login = "LeraSavchu";
+        private string name = "Lera";
+        private string surname = "Sasvchuk";
+        private int age = 18;
+        private const string date = "04.10.2022";
+
+        public void Vivod()
+        {
+            Console.WriteLine($"Login: {login};\nName: {name};\nSurname: {surname};\nAge: {age};\nDate: {date};");
+        }
+    }
+}
